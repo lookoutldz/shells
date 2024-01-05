dfa35e574b167851753e5cbf9778b2b9a0473f12
looko <any-hilltop-0@icloud.com>
2024-01-05 17:43:07 +0800

    Update archlinux_installer_efi_btrfs.sh

--- archlinux/archlinux_installer_efi_btrfs.sh
+++ archlinux/archlinux_installer_efi_btrfs.sh
@@ -109,8 +109,8 @@ cat /mnt/etc/fstab  && \
 # 安装初步完成, 进入配置环节, 检查配置脚本存在性以决定是否自动配置
 INIT_SCRIPT="archlinux_initializer_efi.sh" && \
 if [ ! -e $INIT_SCRIPT ]; then
-    curl -sfL https://raw.githubusercontent.com/lookoutldz/shells/my-n200/archlinux/archlinux_initializer_efi.sh | > $INIT_SCRIPT
-fi && \
+    curl -sfL https://raw.githubusercontent.com/lookoutldz/shells/my-n200/archlinux/archlinux_initializer_efi.sh | > $INIT_SCRIPT && \
+fi
 if [ -e $INIT_SCRIPT ]; then
     chmod +x $INIT_SCRIPT && \
     cp $INIT_SCRIPT /mnt && \
